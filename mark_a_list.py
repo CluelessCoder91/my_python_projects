b3a54976e04b366debd997bd063efe5665d6c461
Liam <103289381+CluelessCoder91@users.noreply.github.com>
2022-08-13 13:58:58 -0700

    *nested-list
    big_list[nested_list][nested_list_index_value] - this is the order to call a value from a specific list.
    This data structure took me A LOT of thinking to fully understand it. The concept of nested-list becomes quite confusing when we have to change values so that normal people can interact with our programs. I had to constantly remind myself that it is the function that we're working with; naming a variable is to assist our understanding, it does not change how a function operates. This one was a difficult one. I need to review it from time to time.

--- mark_a_list.py
+++ mark_a_list.py
@@ -8,7 +8,7 @@ position = input("Where do you want to put the X mark?\n")
 # make sense to start with a zero. To avoid out of range inputs
 # we -1 internally with our codes.
 col = int(position[0]) - 1  # position[0] is the first digit from the user's input variable named 'position'
-row = int(position[1]) - 1  # position[0] is the first digit from the user's input variable named 'position'
+row = int(position[1]) - 1  # position[0] is the second digit from the user's input variable named 'position'
 
 new_map[row][col] = "X"
 print(f"{row1}\n{row2}\n{row3}")
