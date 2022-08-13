578f6284b23611eb4463a803ddc0d3c172063b71
Liam <103289381+CluelessCoder91@users.noreply.github.com>
2022-08-13 12:53:36 -0700

    Delete leap_year_calculator.py

--- leap_year_calculator.py
+++
@@ -1,8 +0,0 @@
-year = int(input("Which year do you want to check? "))
-
-if year % 4 == 0 and year % 100 != 0:
-    print(f"{year} is a leap year.")
-    if year % 100 == 0 and year % 400 == 0:
-        print(f"{year} is not a leap year.")
-else:
-    print(f"{year} is not a leap year.")
