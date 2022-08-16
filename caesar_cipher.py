95d089ca76ca3ba417c32ccd8238e2c68ee8990f
Liam <103289381+CluelessCoder91@users.noreply.github.com>
2022-08-16 11:05:17 -0700

    *fun, difficult, learned a lot!
    This program was really hard. Everything was ok until the 'shift_number' is out-of-bound. Originally, all I could think of was to increase the size of the alphabet, but that solution will restrict the 'shift_number' which I think is an unsatisfying solution; plus it will greatly increase the size of the list just by repeating its value.
    
    I had a talk with my sister and she suggested that I could use modulus if I want the 'shift_number' to stay relative to the original list no matter how great the 'shift_number' is. This was such a great idea. I struggled a lot to find the correct way to implement the idea but eventually got it.
    
    However, now that I understand a little bit more about programming, I'm sure a cipher of this caliber will be cracked within seconds by a loop that checks for all possible combinations from 0 to infinity. Still, this was such a fun program to make. Really enjoy making this program.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -5,38 +5,68 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
-def encrypt(plain_text, shift_amount):
-    encrypted_text = ""
-    for i in plain_text:
-        if i == " ":
-            encrypted_text += " "
-        elif i == ".":
-            encrypted_text += "."
-        else:
-            position = alphabet.index(i)
-            new_position = (position + shift_amount) % 26
-            new_letter = alphabet[new_position]
-            encrypted_text += new_letter
-    print(f"The encrypted text is '{encrypted_text}'.")
-
-
-def decrypt(cipher_text, shift_amount):
-    decrypted_text = ""
-    for i in cipher_text:
-        if i == " ":
-            decrypted_text += " "
-        elif i == ".":
-            decrypted_text += "."
-        else:
-            position = alphabet.index(i)
+# Upgrade to make the code shorter and more intuitive
+def caesar(cipher_direction, start_text, shift_amount):
+    end_text = ""
+    for letter in start_text:
+        if letter not in alphabet:
+            end_text += letter
+        elif cipher_direction == "decode":
+            position = alphabet.index(letter)
             new_position = (position - shift_amount) % 26
-            new_letter = alphabet[new_position]
-            decrypted_text += new_letter
-    print(f"The decrypted text is '{decrypted_text}'.")
+            end_text += alphabet[new_position]
+        elif cipher_direction == "encode":
+            position = alphabet.index(letter)
+            new_position = (position + shift_amount) % 26
+            end_text += alphabet[new_position]
+    print(f"The {direction}d text is: {end_text}")
 
 
-if direction == "encode":
-    encrypt(plain_text = text, shift_amount = shift)
-elif direction == "decode":
-    decrypt(cipher_text = text, shift_amount = shift)
+keep_running = True
+while keep_running is True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(start_text = text, shift_amount = shift, cipher_direction = direction)
+    restart = input("Would you like to restart the cipher? Type 'yes' to proceed, 'no' to exit:\n").lower()
+    if restart == 'no':
+        keep_running = False
+        print("Goodbye!")
+    else:
+        keep_running = True
+
+
+# def encrypt(plain_text, shift_amount):
+#     encrypted_text = ""
+#     for i in plain_text:
+#         if i == " ":
+#             encrypted_text += " "
+#         elif i == ".":
+#             encrypted_text += "."
+#         else:
+#             position = alphabet.index(i)
+#             new_position = (position + shift_amount) % 26
+#             new_letter = alphabet[new_position]
+#             encrypted_text += new_letter
+#     print(f"The encrypted text is '{encrypted_text}'.")
+
+
+# def decrypt(cipher_text, shift_amount):
+#     decrypted_text = ""
+#     for i in cipher_text:
+#         if i == " ":
+#             decrypted_text += " "
+#         elif i == ".":
+#             decrypted_text += "."
+#         else:
+#             position = alphabet.index(i)
+#             new_position = (position - shift_amount) % 26
+#             new_letter = alphabet[new_position]
+#             decrypted_text += new_letter
+#     print(f"The decrypted text is '{decrypted_text}'.")
+
+
+# if direction == "encode":
+#     encrypt(plain_text = text, shift_amount = shift)
+# elif direction == "decode":
+#     decrypt(cipher_text = text, shift_amount = shift)
